a86f79939a108c553297ebddf75cbaafce03db5e
Maxim Zebzeev <maksimzeb@yandex.ru>
2021-07-02 18:58:20 +0500

    change component witch form-person

--- CardPerson/src/app/app.module.ts
+++ CardPerson/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {ReactiveFormsModule} from "@angular/forms";
 
 import { CardPerson } from './card-person/card-person.component';
 import { DescriptionPersonComponent } from "./DescriptionPerson/description-person.component";
@@ -16,7 +17,8 @@ import { FormPersonComponent } from "./form-person/form-person.component";
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- CardPerson/src/app/form-person/form-person.component.ts
+++ CardPerson/src/app/form-person/form-person.component.ts
@@ -1,4 +1,6 @@
 import {Component} from "@angular/core";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {PERSONS} from "../person";
 
 @Component({
   selector: 'app-form-person',
@@ -7,5 +9,14 @@ import {Component} from "@angular/core";
 })
 
 export class FormPersonComponent {
+  cardPerson = new FormGroup({
+    name: new FormControl(''),
+    profession: new FormControl('')
+  })
 
+  send() {
+    console.log(this.cardPerson.value);
+    this.cardPerson.value.name = '';
+    this.cardPerson.value.profession = '';
+  }
 }
